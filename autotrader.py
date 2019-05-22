3c34de50fef62cb9a750e5d048d4b4ca56bce09e
mzaghi <michael_zaghi@sfu.ca>
2019-05-22 01:03:23 -0400

    added run threads

--- autotrader.py
+++ autotrader.py
@@ -2,14 +2,14 @@ import queue
 from bs4 import BeautifulSoup
 import pymysql
 import sys
-from threading import Thread
+from threading import Thread, Lock
 import requests
 import re
 
 class crawler(Thread):
 	#class variables
-	#proxy={'http':'http://200.192.255.102:8080', 'https':'http://200.192.255.102:8080'}
-	header={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+	conn = pymysql.connect(user='root', password='root', host='localhost',database='sys')
+	header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 				AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
 
 	def __init__(self,path):
@@ -23,7 +23,7 @@ class crawler(Thread):
 		self.bsParse = []
 		self.links = []
 		self.vehicles = []
-		
+
 	def gather_links(self):
 		
 		self.bsParse = self.bsObj.findAll('div', {'class':'listing-details'})
@@ -31,7 +31,7 @@ class crawler(Thread):
 			if 'href' in tag.a.attrs: self.links.append('https://www.autotrader.ca'+ tag.a.attrs['href'])
 
 	def update_request(self,link):
-		print(link)
+		#print(link)
 		self.req = requests.get(link,headers=self.header)
 		self.content = self.req.content
 		self.bsObj = BeautifulSoup(self.content,'lxml')
@@ -50,8 +50,27 @@ class crawler(Thread):
 		else:
 			return True
 
+	def update_db(self, data):
+		
+		for row in data:
+
+			sql_update = """INSERT INTO autotrader(adID, condition, make, model, price, province, rawLocation, 
+							year, kilometers, exterior color, fuel type) VALUES('%s','%s','%s','%s',%s,'%s','%s',%s,%s,'%s')"""%(
+							row['adID'], row['condition'], row['make'], row['model'], row['price'], row['province'],
+							row['rawLocation'], row['year'], row['kilometres'], row['fuel type'])
+
+			cursor = self.conn.cursor()
+			cursor.execute(sql_update)
+			self.conn.commit()
+			# try:
+			# 	cursor.execute(sql_update)
+			# 	self.conn.commit()
+			# except:
+			# 	self.conn.rollback()
+
 	def gather_details(self):
 
+		self.vehicles = []
 		vehicle_details = {'adID':'','condition':'','make':'','model':'','price':'','province':'','rawLocation':'',
 							'year':'','kilometres':'','exterior colour':'','fuel type':''}
 
@@ -76,60 +95,77 @@ class crawler(Thread):
 				index = details.index(key)					#TODO: error handling on .index()
 				vehicle_details[key] = details[index+1]
 
-			print(vehicle_details)
+			#print(vehicle_details)
+			self.vehicles.append(vehicle_details)
+
+		self.links = []
+		return self.vehicles
+
+	def run(self):
+		last_page = False
+
+		print('starting {} ...'.format(self.getName()))
+		
+		while (last_page==False):
+
+			self.gather_links()
+			print('{} gathered link details ...'.format(self.getName()))
+			vehicles = self.gather_details()
+			print('{} gathered vehicle details ...'.format(self.getName()))
+
+			lock.acquire()
+			print('{} acquired the lock ...'.format(self.getName()))
+			self.update_db(vehicles)
+			lock.release()
+			print('{} acquired released the lock ...'.format(self.getName()))
+			
+			self.update_request(Q.get())
+			last_page = self.check_page_index()
+			print('{} last page is {} ...'.format(self.getName(),last_page))
+
 		
 def get_q(rng):
-	
-		Q = queue.Queue(-1)
-
+		
+		Q = queue.Queue(1000)
 		for page in range(0,max_index*100,100):
-			
+		
 			url = "https://www.autotrader.ca/cars/?rcp=100&rcs={}&srt=33&pRng={}%2C{}&prx=-1&loc=V3J%203S9&hprc=\
 				True&wcp=True&sts=New-Used&inMarket=advancedSearch\
 				".format(page,price_range[rng][0],price_range[rng][1])
 			Q.put(url)
-
+		
 		return Q
 
-
-
 if __name__ == '__main__':
-
 	'''
 	autotrader.ca search returns a maximum of 1000 indices when 100 postiings per page is set. By breaking the search 
 	into price intervals, this allows the search to stay below the 1000 index max.
 	'''
 
 	#global variables
-	#conn = pymysql.connect(user='root',host='localhost',database='test')
-	
 	price_range = [(1001,10000),(10001,20000),(20001,30000),(30001,40000),(40001,50000),(50001,60000),
 					(60001,70000),(70001,80000),(80001,90000),(90001,100000),(100001,200000),(200001,2000000)]
 	
 	max_index = 1000
 	threads = []
-	set_threads = 10	
+	set_threads = 10
+	lock = Lock()	
 	
 	#main loop
 	for current_range in range(len(price_range)):
+		print('populating queue for range {} ...'.format(price_range[current_range]))
 		Q = get_q(current_range)
-
+		
 		for i in range(set_threads):
 			thread = crawler(Q.get())
+			thread.setName('thread'+str(i))
 			threads.append(thread)
 			thread.start()
-
-
-
-	
+		
 		for thread in threads: thread.join()
 		
 
 	
-	#crawler1 = crawler('https://wwwb.autotrader.ca/cars/bc/coquitlam/?rcp=100&rcs=37100&srt=3&prx=100&prv=British%20Columbia&loc=V3J%203S9&hprc=True&wcp=True&sts=New-Used&inMarket=basicSearch')
-	#flag = crawler1.check_page_index()
-	#print(flag)
-	#crawler1.gather_links()
-	#crawler1.gather_details()
+
 	
 	
