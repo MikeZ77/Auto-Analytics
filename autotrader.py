2f38868e032a3edb3f22fa36537d4a5f0a91b64b
mzaghi <michael_zaghi@sfu.ca>
2019-07-07 21:48:40 -0400

    added proxy rotation

--- autotrader.py
+++ autotrader.py
@@ -6,22 +6,23 @@ from threading import Thread, Lock
 from datetime import datetime
 from proxybroker import Broker
 import asyncio
-import requests
+import logging
 import warnings
+import requests
 import random
 import re
 
 def get_q(rng):
-		
-		Q = queue.Queue(1000)
-		for page in range(0,max_index*100,100):
-		
-			url = "https://www.autotrader.ca/cars/?rcp=100&rcs={}&srt=33&pRng={}%2C{}&prx=-1&loc=V3J%203S9&hprc=\
-				True&wcp=True&sts=New-Used&inMarket=advancedSearch\
-				".format(page,price_range[rng][0],price_range[rng][1])
-			Q.put(url)
-		
-		return Q
+	
+	Q = queue.Queue(1000)
+	for page in range(0,max_index*100,100):
+	
+		url = "https://www.autotrader.ca/cars/?rcp=100&rcs={}&srt=33&pRng={}%2C{}&prx=-1&loc=V3J%203S9&hprc=\
+			True&wcp=True&sts=New-Used&inMarket=advancedSearch\
+			".format(page,price_range[rng][0],price_range[rng][1])
+		Q.put(url)
+	
+	return Q
 
 def get_proxies(num):
 
@@ -38,10 +39,16 @@ def get_proxies(num):
 		broker.find(types=['HTTPS'], limit=num, countries=['US','CA']),
 		show(proxies))
 
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(tasks)
-	print(proxy_list)
-	return proxy_list
+	try: 
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(tasks)
+	except:
+		print("CLOSING ...")
+		sys.exit(-1)
+	finally:
+		print(proxy_list)
+		return proxy_list
+
 
 def parse_proxies(proxy_list, protocol):
 	parsed_list = list();
@@ -64,8 +71,9 @@ class crawler(Thread):
 		   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15]']
 
 	conn = mysql.connector.connect(user='root', passwd='root', host='localhost',database='sys')
+	proxies = None
 
-	def __init__(self, proxies):
+	def __init__(self, outside_proxy, main_Q, worker_Q):
 		Thread.__init__(self)
 		#instance variables
 		#connection/page objects
@@ -73,10 +81,14 @@ class crawler(Thread):
 		self.content = None
 		self.bsObj = None
 		self.path = None
-		#structures
+		self.init_proxies(outside_proxy)
+		#page structures
 		self.bsParse = []
 		self.links = []
 		self.vehicles = []
+		#proxy structures
+		self.main_Q = main_Q
+		self.worker_Q = worker_Q
 
 	def gather_links(self):
 
@@ -84,25 +96,26 @@ class crawler(Thread):
 		for tag in self.bsParse:
 			if 'href' in tag.a.attrs: self.links.append('https://www.autotrader.ca'+ tag.a.attrs['href'])
 
+	def init_proxies(self, outside_proxy):
+		self.proxies = outside_proxy
+
 	def update_request(self,link):
-		#self.req = requests.get(link,headers={'user-agent':random.choice(self.headers)})
+
 		self.req = None
 		while self.req is None:
 			
 			try:
-				proxy = random.choice(proxies)
+				proxy = random.choice(self.proxies)
 				self.req = requests.get(link,headers={'user-agent':random.choice(self.headers)}, proxies={'https':proxy}, timeout=10)
 			
 			except (requests.exceptions.Timeout, requests.exceptions.ConnectTimeout):
 				print("{} connection timeout using ip: {} ... Dropping from proxies ...".format(self.getName(), proxy))
-				if proxy in proxies: 
-					proxies.remove(proxy)
-					#print("Count" + str(len(proxies)))
+				if proxy in self.proxies: 
+					self.proxies.remove(proxy)
 			except requests.exceptions.RequestException:
 				print("{} other connection issue using ip: {} ... Dropping from proxies ...".format(self.getName(), proxy))
-				if proxy in proxies: 
-					proxies.remove(proxy)
-					#print("Count" + str(len(proxies)))
+				if proxy in self.proxies: 
+					self.proxies.remove(proxy)
 
 		self.content = self.req.content
 		self.bsObj = BeautifulSoup(self.content,'lxml')
@@ -156,15 +169,7 @@ class crawler(Thread):
 
 		self.vehicles = []
 
-		#need to error log if links == 0
-		print("Links: ")
-		print(self.path)
-		print(len(self.links))
-		if len(self.links)==0: 
-			print("printing 0 bsparse")
-			print(self.bsParse)
-			print(self.bsObj)
-		
+		#TODO: Include error handling for empty links
 		for link in self.links:
 
 			vehicle_details = {'adID':'','condition':'','make':'','model':'','price':'','province':'','rawLocation':'',
@@ -176,7 +181,7 @@ class crawler(Thread):
 			try:
 				details = re.sub('"','',self.bsParse[2].text)
 			except:
-				print("gtmManager.initializeDataLayer method is not available in source for this listing")
+				logging.warning("gtmManager.initializeDataLayer method is not available in source for this listing at url: {}".format(link))
 				continue
 
 			details = re.split(',|:|{',details)
@@ -213,16 +218,28 @@ class crawler(Thread):
 			vehicles = self.gather_details()
 			print('{} gathered vehicle details ...'.format(self.getName()))
 
-			lock.acquire()
+			db_lock.acquire()
 			print('{} acquired the lock ...'.format(self.getName()))
 			self.update_db(vehicles)
-			lock.release()
+			db_lock.release()
 			print('{} released the lock ...'.format(self.getName()))
+
+			#Rotate fresh proxies
+			#-----------------------------------------------------------------
+			if (len(self.proxies) <= 5) and (proxy_lock.acquire(False)):
+				print('{} rotating fresh proxies ...'.format(self.getName()))
+				self.main_Q.put(True)
+				fresh_proxies = self.worker_Q.get()
+				self.proxies += fresh_proxies
+				proxy_lock.release()
+			#-----------------------------------------------------------------
 
 			self.path = Q.get()
 			self.update_request(self.path)
 
 			last_page = self.check_page_index()
+			#stop main loop from listening for fresh proxies
+			if last_page: self.main_Q.put(False)
 			print('{} last page is {} ...'.format(self.getName(),last_page))
 
 
@@ -236,13 +253,19 @@ if __name__ == '__main__':
 					(60001,70000),(70001,80000),(80001,90000),(90001,100000),(100001,200000),(200001,2000000)]
 
 	max_index = 1000
-	threads = []
+	num_proxies = 10
+	cycled_proxies = 5
 	set_threads = 10
-	lock = Lock()
-	lock_proxies = Lock()
-	num_proxies = 30
+	threads = []
+	db_lock = Lock()
+	proxy_lock = Lock()
+	main_Q = queue.Queue()
+	worker_Q = queue.Queue()
+	warnings.simplefilter("error", UserWarning)
+	logging.basicConfig(filename='error.log',level=logging.INFO,format='%(asctime)s:%(threadName)s:%(levelname)s:%(message)s')
 
 	#main loop
+	logging.info("STARTING NEW ITERATION")
 	for current_range in range(len(price_range)):
 		print('-----------------------------------------------------------------------------')
 		print('populating queue for range {} ...'.format(price_range[current_range]))
@@ -250,19 +273,30 @@ if __name__ == '__main__':
 		Q = get_q(current_range)
 		print('retrieving proxies ...')
 		print('-----------------------------------------------------------------------------')
-		
 	
 		proxies = get_proxies(num_proxies)
 		proxies = parse_proxies(proxies, 'https')
 	
 		for i in range(set_threads):
-			thread = crawler(proxies)
+			thread = crawler(proxies,main_Q,worker_Q)
 			thread.setName('thread'+str(i))
 			threads.append(thread)
 			thread.start()
+
+		while True:
+		#listen for fresh proxy request from thread
+			fresh_proxies = main_Q.get()
+			if fresh_proxies:
+				fresh_proxies = get_proxies(cycled_proxies)
+				fresh_proxies = parse_proxies(fresh_proxies, 'https')
+				worker_Q.put(fresh_proxies)
+			else:
+				break
 		
+		#block until all threads are joined
 		for thread in threads: thread.join()
 	
+	logging.info("COMPLETED")
 	sys.exit(0)
 
 
